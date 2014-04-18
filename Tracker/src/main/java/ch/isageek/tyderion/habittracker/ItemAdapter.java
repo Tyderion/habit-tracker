bf244e27b713f968d3b8d955518afe0276ce3ceb
Gabriel Nadler <nadlergabriel@gmail.com>
2014-04-18 21:55:33 +0200

    add new detail-activity

---
+++ Tracker/src/main/java/ch/isageek/tyderion/habittracker/ItemAdapter.java
@@ -0,0 +1,18 @@
+package ch.isageek.tyderion.habittracker;
+
+import android.content.ClipData;
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import ch.isageek.tyderion.habittracker.model.Habit;
+
+/**
+ * Created by Archie on 18.04.2014.
+ */
+public class ItemAdapter extends ArrayAdapter<Habit> {
+
+
+    public ItemAdapter(Context context, int resource, Habit[] objects) {
+        super(context, resource, objects);
+    }
+}
